2492e1285a7dc5b8a9477ec8766cfe2b2dce221c
Satılmış TUNÇ <tunc0687@hotmail.com>
2022-07-16 18:12:57 +0300

    Add DataAccess

---
+++ N-TierArchitectureToDoApp.DataDomain/EfCoreUnitOfWork/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using N_TierArchitectureToDoApp.DataDomain.DbContexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N_TierArchitectureToDoApp.DataDomain.EfCoreUnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ToDoAppDbContext _context;
+
+        public UnitOfWork(ToDoAppDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ N-TierArchitectureToDoApp.DataDomain/Entities/Work.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace N_TierArchitectureToDoApp.DataDomain.Entities
+{
+    public partial class Work
+    {
+        public int Id { get; set; }
+        public string Description { get; set; } = null!;
+        public bool IsCompleted { get; set; }
+    }
+}
