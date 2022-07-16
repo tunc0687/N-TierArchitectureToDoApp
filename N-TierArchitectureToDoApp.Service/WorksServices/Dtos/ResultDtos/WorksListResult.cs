3adb398c9a70c9553661eef6097e960b4912a4c4
Satılmış TUNÇ <tunc0687@hotmail.com>
2022-07-16 19:13:23 +0300

    Add Dtos

---
+++ N-TierArchitectureToDoApp.Service/WorksServices/Dtos/ResultDtos/WorksListResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N_TierArchitectureToDoApp.Service.WorksServices.Dtos.ResultDtos
+{
+    public class WorksListResult
+    {
+        public int Id { get; set; }
+        public string Description { get; set; } = null!;
+        public bool IsCompleted { get; set; }
+    }
+}
---
+++ N-TierArchitectureToDoApp.Service/WorksServices/IWorksService.cs
@@ -0,0 +1,19 @@
+﻿using N_TierArchitectureToDoApp.Service.WorksServices.Dtos.RequestDtos;
+using N_TierArchitectureToDoApp.Service.WorksServices.Dtos.ResultDtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N_TierArchitectureToDoApp.Service.WorksServices
+{
+    public interface IWorksService
+    {
+        Task<int> Add(WorksAddRequest request);
+        Task UpdateById(int id, WorksUpdateByIdRequest request);
+        Task DeleteById(int id);
+        Task<WorksGetByIdResult> GetById(int id);
+        Task<List<WorksListResult>> GetAll();
+    }
+}
